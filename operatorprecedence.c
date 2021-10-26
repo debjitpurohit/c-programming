35a272afff53e818be092994021c12028e72c995
DEBJIT PUROHIT <DEBJITPUROHIT2001@outlook.com>
2021-10-26 11:59:56 +0530

    mistake

--- operatorprecedence.c
+++ operatorprecedence.c
@@ -4,7 +4,8 @@ int main(){
     int x=4;
     int y =6;
     printf("%d\n",3*x-8*y);
-    printf("%d",8*y/3*x);  // gives wrong answer then comes associativity bcz * and / have same priority
+    printf("%d",8*y/3*x);  // 8*6=48, 48/3=16,16*4=64 gives wrong answer then comes associativity bcz * and / have same priority
+    // always maintain left to right not follow bodmas according to math
     
     return 0;
 }
