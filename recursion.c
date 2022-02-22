41c5400130d04f2bc7e2880aad13aaf9bdda5c53
DEBJIT PUROHIT <DEBJITPUROHIT2001@outlook.com>
2022-02-22 10:06:23 +0530

    right

--- recursion.c
+++ recursion.c
@@ -3,11 +3,11 @@
 //factorial(5)=5*4*3*2*1
 //factorial(n)=n*n-1*n-2*n-3*n-4*.....
 // or....factorial(5)=1*2*3*4*5
-// thats like ...factorioal(n)=1*2*3*n-3*n-2*n-1*n
+// thats like ...factorioal(n)=1*n-3*n-2*n-1*n
 //factorial(0)=1 
 // factorial(1)=1
-//factorial(n)=n*n-1*n-2*n-3*n-4*3*2*1
-//factorial(n)=n*(n-1*n-2*n-3*n-4*3*2*1)
+//factorial(n)=n*n-1*n-2*n-3*n-4........*3*2*1
+//factorial(n)=n*(n-1*n-2*n-3*n-4*.....)
 //==factorial(n)=n*fctorial(n-1)
 int factorial(int);
 int main(){
